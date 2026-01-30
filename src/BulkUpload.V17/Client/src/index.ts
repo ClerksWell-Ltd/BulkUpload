50893e6539c6c8b4c4a71b607f580f7a6eeeabe4
Paul Seal <prjseal@gmail.com>
2026-01-30 13:14:37 +0000

    first v17 frontend build

---
+++ src/BulkUpload.V17/Client/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Bulk Upload for Umbraco 17
+ * Entry point for extension registration
+ */
+
+export { manifests } from './manifests.js';
+export * from './components/bulk-upload-dashboard.element.js';
